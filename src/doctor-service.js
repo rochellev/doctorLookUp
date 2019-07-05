a2bc581fe0d335e0c55f42ad0a41de7c62186f0f
rochellev <rochellevroberts@utexas.edu>
2019-07-05 10:55:02 -0700

    calling api correctly and displaying information

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,26 +1,23 @@
 export class DoctorService {
-    // returns propise 
     getDoctorByName(firstName, lastName, city){
-
-    }
-
-    getDoctorByPractice(practice){
-
-    }
-
-    getWeatherByCity(city) {
         return new Promise(function(resolve, reject) {
-          let request = new XMLHttpRequest();
-          let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
-          request.onload = function() {
-            if (this.status === 200) {
-              resolve(request.response);
-            } else {
-              reject(Error(request.statusText));
+            let request = new XMLHttpRequest();
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=${city}&sort=last-name-asc&skip=0&limit=10&user_key=f32a294c81862966871f24cf3fb8246a`;
+            request.onload = function() {
+              if (this.status === 200) {
+                resolve(request.response);
+              } else {
+                reject(Error(request.statusText));
+              }
             }
-          }
-          request.open("GET", url, true);
-          request.send();
-        });
-      }
+            request.open("GET", url, true);
+            request.send();
+          });
+
+    }
+
+    // getDoctorByPractice(practice){
+
+    // }
+
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { DoctorService } from './doctor-service';
 
 
 $(document).ready(function() {
-  
+  $('#searchName').click(function(){
+      let firstName = $('#firstName').val();
+      let lastName = $('#lastName').val();
+      let city = "wa-seattle";
+      let doctorService = new DoctorService();
+      let promise = doctorService.getDoctorByName(firstName, lastName, city);
+
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        $('#doctorResult').text(`Your Doctor in ${city} name is ${body.data[0].practices[0].name} and address is ${body.data[0].practices[0].visit_address.street}`);
+      }, function(error) {
+        $('#showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+
+      
+  })
 });
