ad5b57fd77bec1b250746931eb4ec77dccd4ddd0
rochellev <rochellevroberts@utexas.edu>
2019-07-05 09:54:45 -0700

    DoctorService class for calling the api

---
+++ src/doctor-service.js
@@ -0,0 +1,26 @@
+export class DoctorService {
+    // returns propise 
+    getDoctorByName(firstName, lastName, city){
+
+    }
+
+    getDoctorByPractice(practice){
+
+    }
+
+    getWeatherByCity(city) {
+        return new Promise(function(resolve, reject) {
+          let request = new XMLHttpRequest();
+          let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+          request.onload = function() {
+            if (this.status === 200) {
+              resolve(request.response);
+            } else {
+              reject(Error(request.statusText));
+            }
+          }
+          request.open("GET", url, true);
+          request.send();
+        });
+      }
+}
