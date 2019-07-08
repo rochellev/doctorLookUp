24b5ec5ed4086cdc40622a28ed5bed0fe550e74f
Student <example@example.com>
2019-07-08 16:01:41 -0700

    added symptom search

--- src/main.js
+++ src/main.js
@@ -35,8 +35,52 @@ $(document).ready(function () {
             $('#showErrors').text(`There was an error processing your request: ${error.message}`);
         });
     });
-    // searching by Doctor Practice Type
-    $('#searchType').click(function () {
+    // searching by symptom
+    $('#symptomSearch').click(function () {
+        let doctorType = "";
+        let symptom = $("#symptoms").val();
+        if(symptom == "Rash"){
+            doctorType = "Dermatologist";
+        }else{
+            doctorType = symptom;
+        }
+        let city = "wa-seattle"; // hard coded for now
+        let doctorService = new DoctorService();
+        let promise = doctorService.getDoctorByPractice(doctorType, city);
+
+        promise.then(function (response) {
+            let body = JSON.parse(response);
+            if(body.data.length == 0){
+                $('#doctorResult').text(`Sorry, no results foryour search. Please try again!`);
+            }
+            let current = 0;
+            let takingNew = "";
+            let website= "";
+            for (let d = 0; d < body.data.length; d++) {
+                current = d;
+                for (let i = 0; i < body.data[current].practices.length; i++) {
+                    if (body.data[current].practices[i].accepts_new_patients) {
+                        takingNew = "Yes";
+                    } else {
+                        takingNew = "No";
+                    }
+                    if(body.data[current].practices[i].website){
+                        website = "body.data[current].practices[i].website";
+                    }else{
+                        website = "Website not available";
+                    }
+
+                    $('#doctorResult').prepend(`<div>Name: ${body.data[current].practices[i].name}<br> Address: ${body.data[current].practices[i].visit_address.street},${body.data[current].practices[i].visit_address.city},${body.data[current].practices[i].visit_address.state}  ${body.data[current].practices[i].visit_address.zip}<br> Phone: ${body.data[current].practices[i].phones[0].number}<br> Accepting New Patients: ${takingNew}<br> Website: ${website}</div><br>`);
+                }
+
+            }
+        }, function (error) {
+            $('#showErrors').text(`There was an error processing your request: ${error.message}`);
+        });
+    })
+
+     // searching by Doctor Practice Type
+     $('#searchType').click(function () {
         let doctorType = $('#doctorTypes').val();
         let city = "wa-seattle"; // hard coded for now
         let doctorService = new DoctorService();
@@ -77,4 +121,5 @@ $(document).ready(function () {
 
 
     })
+
 });
